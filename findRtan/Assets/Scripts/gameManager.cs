5fa66f5199205215069fb8ee5d9bf11827b4f8da
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-01-19 23:36:20 +0900

    카드 오브젝트 애니메이션 추가 - 대기 상태 카드 애니메이션 추가 - 카드 뒤집는 애니메이션과 대기상태로 돌아가는 트랜잭션 추가

--- findRtan/Assets/Scripts/gameManager.cs
+++ findRtan/Assets/Scripts/gameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +9,27 @@ public class gameManager : MonoBehaviour
     public Text timeText;
     public GameObject card;
     float time;
+    int cardKinds = 8;
     // Start is called before the first frame update
     void Start()
     {
+        List<int> rtans = new List<int>();
+        for (int i=0; i<cardKinds*2; i++) {
+            rtans.Add(i/2);
+        }
+
+        rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToList();
+
         for (int i=0; i<16; i++) {
             GameObject newCard = Instantiate(card);
             newCard.transform.parent = GameObject.Find("cards").transform;
+
             float x = i % 4 * 1.4f - 2.1f;
             float y = i / 4 * 1.4f - 3.0f;
             newCard.transform.position = new Vector3(x, y, 0);
+
+            string rtanName = "rtan" + rtans[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(rtanName);
         }
     }
 
